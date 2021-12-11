9eb8799fa431325d105fd1699f9d7e4935504b85
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-11 18:08:33 +0000

    Create Population.java

---
+++ BGAj/src/main/java/com/barengific/bgaj/Population.java
@@ -0,0 +1,12 @@
+package com.barengific.bgaj;
+
+import java.util.ArrayList;
+
+public class Population {
+    ArrayList<Chromosome> cPop;
+    int pSize;
+
+    public int getPSize(){
+        return cPop.size();
+    }
+}
