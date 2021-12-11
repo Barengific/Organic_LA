9ceca250afa648b863e779eb7248f231acc8e4ce
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-11 18:08:09 +0000

    Create Chromosome.java

---
+++ BGAj/src/main/java/com/barengific/bgaj/Chromosome.java
@@ -0,0 +1,25 @@
+package com.barengific.bgaj;
+
+public class Chromosome {
+    String chromosome;
+    int cSize;
+
+    public String getChromosome() {
+        return chromosome;
+    }
+
+    public void setChromosome(String chromosome) {
+        this.chromosome = chromosome;
+    }
+
+    public int getcSize() {
+        return cSize;
+    }
+
+    public void setcSize(int cSize) {
+        this.cSize = cSize;
+    }
+
+    //TODO convert chromosome to decinal value
+
+}
