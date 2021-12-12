9da68c29e54da08368bf7cdafe001170499a6ef5
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-12 16:12:29 +0000

    Create Base58.java

---
+++ BGAj/src/main/java/com/barengific/bgaj/Base58.java
@@ -0,0 +1,10 @@
+package com.barengific.bgaj;
+
+public class Base58 {
+    String codeString = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+    String out(String gene){
+
+        return null;
+    }
+}
