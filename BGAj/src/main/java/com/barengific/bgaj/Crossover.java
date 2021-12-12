6cfff63a6f99efe3c1c680304f6e43a6902c5db2
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-12 14:30:59 +0000

    Create Crossover.java

---
+++ BGAj/src/main/java/com/barengific/bgaj/Crossover.java
@@ -0,0 +1,30 @@
+package com.barengific.bgaj;
+
+import java.util.ArrayList;
+
+public class Crossover {
+
+    ArrayList<String> singlePoint(ArrayList<String> a){
+        //TODO : Step 1: A crossover point is randomly selected between the first and last bits of the parents’
+        //chromosomes.
+        //Step 2: Generate two offspring by swapping the chromosomes from the crossover point between
+        //two parents.
+        //Step 3: Replace any two chromosomes to be discarded in the pool of Npop −Nkeep in the
+        //population.
+        //Step 4: Repeat Steps 1 to 3 for the next two parents until the pool of Npop −Nkeep is replaced
+        return null;
+    }
+
+    ArrayList<String> doublePoint(ArrayList<String> a){
+        //TODO : The segments in between two randomly
+        //generated crossover points are swapped between parents.
+        return null;
+    }
+
+    ArrayList<String> uniform(ArrayList<String> a){
+        //TODO : Bits are randomly chosen for swapping between
+        //parents
+        return null;
+    }
+
+}
