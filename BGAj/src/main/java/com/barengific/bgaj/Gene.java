6e7d382615cf6ece3f9e5a3c3a74968479268d93
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-12 16:15:14 +0000

    Create Gene.java

---
+++ BGAj/src/main/java/com/barengific/bgaj/Gene.java
@@ -0,0 +1,40 @@
+package com.barengific.bgaj;
+
+public class Gene {
+    String gene;
+    int geneSize;
+    int nGenes;
+    double geneRating;
+
+    public String getGene() {
+        return gene;
+    }
+
+    public void setGene(String gene) {
+        this.gene = gene;
+    }
+
+    public int getGeneSize() {
+        return geneSize;
+    }
+
+    public void setGeneSize(int geneSize) {
+        this.geneSize = geneSize;
+    }
+
+    public int getnGenes() {
+        return nGenes;
+    }
+
+    public void setnGenes(int nGenes) {
+        this.nGenes = nGenes;
+    }
+
+    public double getGeneRating() {
+        return geneRating;
+    }
+
+    public void setGeneRating(double geneRating) {
+        this.geneRating = geneRating;
+    }
+}
