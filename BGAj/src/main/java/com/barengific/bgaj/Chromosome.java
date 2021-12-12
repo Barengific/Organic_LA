367570ec0275a450bd37c96552dcce3bac666493
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-12 16:15:08 +0000

    Update Chromosome.java

--- BGAj/src/main/java/com/barengific/bgaj/Chromosome.java
+++ BGAj/src/main/java/com/barengific/bgaj/Chromosome.java
@@ -2,8 +2,6 @@ package com.barengific.bgaj;
 
 public class Chromosome {
     String chromosome;
-    int nGenes;
-    int geneSize;
     int cRating;
 
     public String getChromosome() {
@@ -14,14 +12,6 @@ public class Chromosome {
         this.chromosome = chromosome;
     }
 
-    public int getGeneSize() {
-        return geneSize;
-    }
-
-    public void setGeneSize(int geneSize) {
-        this.geneSize = geneSize;
-    }
-
     public int getcRating() {
         return cRating;
     }
@@ -30,13 +20,6 @@ public class Chromosome {
         this.cRating = cRating;
     }
 
-    public int getnGenes() {
-        return nGenes;
-    }
-
-    public void setnGenes(int nGenes) {
-        this.nGenes = nGenes;
-    }
     //TODO convert chromosome to decimal value
 
 }
