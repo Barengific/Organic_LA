5bc56aa1ad85c817700ad5ca435073d25b684f39
Barengific <75044077+Barengific@users.noreply.github.com>
2021-12-12 14:31:07 +0000

    Create Mutations.java

---
+++ BGAj/src/main/java/com/barengific/bgaj/Mutations.java
@@ -0,0 +1,22 @@
+package com.barengific.bgaj;
+
+import java.util.ArrayList;
+
+public class Mutations {
+    boolean elitism;
+
+    ArrayList<String> b(ArrayList<String> a){
+        //TODO : mutate the chromosome
+        // Purposes: Random mutations alter a certain percentage of the bits in
+        //the list of chromosomes. It allows the GA to explore a cost surface by
+        //introducing new information.
+        //Mutation process:
+        //Step 1: Choose the mutation rate (or probability of mutation), m (0 to 1).
+        //Step 2: Determine the number of bits to be mutated:
+        // Step 3: Flip the chosen bits:
+        return null;
+    }
+
+
+
+}
